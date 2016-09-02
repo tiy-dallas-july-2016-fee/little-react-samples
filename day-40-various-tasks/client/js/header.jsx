e1032eca5a32ab064e9181cec22a84eafd3bc4c8
Eric Sowell <eric.sowell@gmail.com>
2016-09-02 17:23:19 -0500

    initial commit for day 40 various

---
+++ day-40-various-tasks/client/js/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  render () {
+    return <header>
+        <h1>React Header!</h1>
+      </header>;
+  }
+}
+
+exports.Header = Header;
