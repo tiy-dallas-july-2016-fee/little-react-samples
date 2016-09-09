92adde54122196d6ee1b14073e91345e40aef0c0
Eric Sowell <eric.sowell@gmail.com>
2016-09-08 21:16:18 -0500

    file tweaks

--- day-44-flux/client/js/header.jsx
+++ day-44-flux/client/js/header.jsx
@@ -1,11 +1,11 @@
-var React = require('react');
+import React from 'react';
 
 class Header extends React.Component {
 
   render () {
     return (
       <header>
-        <h1>React Header!</h1>
+        <h1>Header!</h1>
       </header>
     );
   }
