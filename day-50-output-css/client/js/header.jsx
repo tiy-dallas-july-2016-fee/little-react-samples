7759198eef0e6dd86ac1472866cf130d38e2a09c
Eric Sowell <eric.sowell@gmail.com>
2016-09-16 06:55:59 -0500

    initial checkin for output-css example

---
+++ day-50-output-css/client/js/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  render () {
+    return (
+      <header>
+        <h1>Header!</h1>
+      </header>
+    );
+  }
+}
+
+module.exports = Header;
